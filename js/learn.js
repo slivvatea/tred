057cacebdb716182238a017d6dbe5f11cbef34cb
anton <ex@mail.com>
2021-10-19 12:29:12 +0500

    start movie project

--- js/learn.js
+++ js/learn.js
@@ -1,10 +1,17 @@
 "use strict";
 
-console.log(4 + +'5');
-
-let incr = 10,
-    decr = 10;
-
-// incr++;
-// decr--;
-console.log(++incr,' ',decr--);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMoviesDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false,
+};
+const a1 = prompt('Один из последних просмотренных фильмов?',''),
+    b1 = prompt('НАсколько оцените его?','');
+personalMoviesDB.movies[a1] = b1;
+const a2 = prompt('Один из последних просмотренных фильмов?',''),
+    b2 = prompt('НАсколько оцените его?','');
+personalMoviesDB.movies[a2] = b2;
+console.log(personalMoviesDB);
