9fad690748d83e78e4afe5f1d2cb56cc404e9a53
anton <ex@mail.com>
2021-10-23 09:54:13 +0500

    forof

--- js/learn.js
+++ js/learn.js
@@ -1,6 +1,16 @@
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms; // = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
 const personalMoviesDB = {
     count : numberOfFilms,
     movies : {},
@@ -9,27 +19,47 @@ const personalMoviesDB = {
     privat : false,
 };
 
-for (let i = 0; i < 2; i++) {
-    const a1 = prompt('Один из последних просмотренных фильмов?',''),
-        b1 = prompt('НАсколько оцените его?','');
-    if ((a1 != '') && (b1 != '') && (a1 != null) && (b1 != null) && (a1.length < 50) && (b1.length < 50)) {
-        
-        personalMoviesDB.movies[a1] = b1; 
-    }else{
-        console.log('error');
-        i--; 
+
+function rememberFilms(params) {
+    for (let i = 0; i < 2; i++) {
+        const a1 = prompt('Один из последних просмотренных фильмов?',''),
+            b1 = prompt('НАсколько оцените его?','');
+        if ((a1 != '') && (b1 != '') && (a1 != null) && (b1 != null) && (a1.length < 50) && (b1.length < 50)) {
+            
+            personalMoviesDB.movies[a1] = b1; 
+        }else{
+            console.log('error');
+            i--; 
+        }
     }
 }
-console.log(personalMoviesDB);
+rememberFilms();
 
-
-
-if (personalMoviesDB.count < 10) {
-    alert('просмотренно мало фильмов');
-}else if ((personalMoviesDB.count > 10) && (personalMoviesDB.count < 30)) {
-    alert('Вы классический зритель');
-}else if (personalMoviesDB.count > 30) {
-    alert('Вы киноман');
-}else{
-    alert('Произошла ошибка');
+function detectedPerslevel(params) {
+    if (personalMoviesDB.count < 10) {
+        alert('просмотренно мало фильмов');
+    }else if ((personalMoviesDB.count > 10) && (personalMoviesDB.count < 30)) {
+        alert('Вы классический зритель');
+    }else if (personalMoviesDB.count > 30) {
+        alert('Вы киноман');
+    }else{
+        alert('Произошла ошибка');
+    }
 }
+detectedPerslevel();
+
+function showMyDB() { 
+    if (personalMoviesDB.privat == false){
+        console.log(personalMoviesDB);
+    }
+ }
+ showMyDB();
+
+ function writeYourGenres() {
+     for (let i = 0; i < 3; i++) {
+        let p = prompt(`Ваш любимый жанр по номеру ${i+1}?`);
+        personalMoviesDB.genres[i] = p;
+     }   
+     console.log(personalMoviesDB);
+ }
+ writeYourGenres();
--- js/menu.js
+++ js/menu.js
@@ -9,11 +9,48 @@ console.log(str.indexOf('q'));
 
 let neCoonst = 'Sero Buryi Color'; 
 console.log(neCoonst.slice(4));
-console.log(neCoonst.substring(6,12));
-console.log(neCoonst.substr(1,6));
-
 const calc = (a,b) => { return a+b };
 
-const num = "12.49px";
-console.log(parseInt(num));
-console.log(parseFloat(num));
+function learn(lang,callback) {  
+    console.log(`I learn ${lang}`);
+    callback();
+}
+learn('JS', function () { 
+    console.log('Ive learned this');
+ })
+
+ const options = {
+     name : 'test',
+     width : 1024,
+     height: 1024,
+     colors: {
+         border: 'black',
+         background: 'red',
+     },
+     maleTest: function () { 
+         console.log("Test");
+      }
+ }
+options.maleTest();
+
+const {border,background} = options.colors;
+console.log(border);
+
+
+// console.log(Object.keys(options).length);
+
+
+// let counter = 0;
+//  for (let key in options){
+//      if (typeof(options[key] === 'Object')) {
+//          for (let i in options[key]){
+//             console.log(`Свойство ${key} имеет значение ${options[key][i]}`);
+//             counter++;
+
+//         }
+//      }else{
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//      }
+//    } 
+// console.log(counter);
